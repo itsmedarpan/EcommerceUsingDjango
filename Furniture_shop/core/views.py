647b21eff9a84caf98fb1793273c1b1164b2e48e
Darpan Adhikari <heyitsmedarpan@gmail.com>
2025-06-05 23:52:09 +0545

    version_0.3

--- Furniture_shop/core/views.py
+++ Furniture_shop/core/views.py
@@ -1,5 +1,6 @@
+from django.db.models import Q
 from django.shortcuts import render
-from product.models import Product
+from product.models import Product, Category
 
 def homepage(request):
     products = Product.objects.all()[0:8]
@@ -8,5 +9,23 @@ def homepage(request):
 
 def shop(request):
     products = Product.objects.all()
+    categories = Category.objects.all()
 
-    return render(request, 'core/shop.html', {'products':products})
+    active_category = request.GET.get('category', '')
+
+    if active_category:
+        products = products.filter(category__slug=active_category)
+
+    query = request.GET.get('query', '')
+
+    if query:
+        products = products.filter(Q(name__icontains=query) | Q(description__icontains=query))
+
+
+    context = {
+        'categories':categories,
+        'products':products,
+        'active_category':active_category
+    }
+
+    return render(request, 'core/shop.html', context)
