5eec83809aa1ec5c0a93a46fa73668d402b114cb
Darpan Adhikari <heyitsmedarpan@gmail.com>
2025-06-07 00:35:33 +0545

    login_and_signup

--- Furniture_shop/core/urls.py
+++ Furniture_shop/core/urls.py
@@ -1,9 +1,16 @@
+from django.conf import settings
+from django.conf.urls.static import static
+
 from django.urls import path, include
-from .views import homepage, shop
+from .views import homepage, shop, signup, login
 from product.views import product
+from cart.views import add_to_cart
 
 urlpatterns = [
     path('',homepage, name='homepage'),
     path('shop/', shop, name='shop'),
     path('shop/<slug:slug>/', product, name='product'),
-]
+    path('add_to_cart/<int:product_id>/', add_to_cart, name='add_to_cart'),
+    path('signup/', signup, name='signup'),
+    path('login/', login, name='login'),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- Furniture_shop/core/views.py
+++ Furniture_shop/core/views.py
@@ -7,6 +7,12 @@ def homepage(request):
 
     return render(request,'core/frontpage.html',{'products':products})
 
+def signup(request):
+    return render(request, 'core/signup.html')
+
+def login(request):
+    return render(request, 'core/login.html')
+
 def shop(request):
     products = Product.objects.all()
     categories = Category.objects.all()
