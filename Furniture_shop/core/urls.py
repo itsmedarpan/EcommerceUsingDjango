84e5a21e276e1e5729389ca3a32ca310e4d21341
Darpan Adhikari <heyitsmedarpan@gmail.com>
2025-06-07 23:44:10 +0545

    authentication_added

--- Furniture_shop/core/urls.py
+++ Furniture_shop/core/urls.py
@@ -2,7 +2,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 from django.urls import path, include
-from .views import homepage, shop, signup, login
+from .views import homepage, shop, signup, login_old
 from product.views import product
 from cart.views import add_to_cart
 
@@ -12,5 +12,5 @@ urlpatterns = [
     path('shop/<slug:slug>/', product, name='product'),
     path('add_to_cart/<int:product_id>/', add_to_cart, name='add_to_cart'),
     path('signup/', signup, name='signup'),
-    path('login/', login, name='login'),
+    path('login/', login_old, name='login'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
