2b3a86560303dbca0f62ab1724ff15d748f37f12
Darpan Adhikari <heyitsmedarpan@gmail.com>
2025-06-12 22:45:00 +0545

    >

--- Furniture_shop/Furniture_shop/settings.py
+++ Furniture_shop/Furniture_shop/settings.py
@@ -46,7 +46,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'core',
     'product',
-    'cart'
+    'cart',
+    'order',
 ]
 
 MIDDLEWARE = [
--- Furniture_shop/cart/urls.py
+++ Furniture_shop/cart/urls.py
@@ -6,8 +6,8 @@ urlpatterns = [
     path('',cart, name='cart'),
     path('add_to_cart/<int:product_id>/', add_to_cart, name='add_to_cart'),
     path('checkout/', checkout, name='checkout'),
-    path('update_cart/<int:product_id>/<str:action>/', update_cart, name='update-cart'),
+    path('update_cart/<int:product_id>/<str:action>/', update_cart, name='update_cart'),
     path('hx_menu_car/', hx_menu_cart, name='hx_menu_cart'),
-    path('hx_cart_total/', hx_menu_cart, name='hx_cart_total'),
+    path('hx_cart_total/', hx_cart_total, name='hx_cart_total'),
 
 ]
