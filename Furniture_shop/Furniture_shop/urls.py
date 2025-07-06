f376f56ceedb9dedff937f416f678b20df7ca55a
Darpan Adhikari <heyitsmedarpan@gmail.com>
2025-07-06 12:44:55 +0545

    .

--- Furniture_shop/Furniture_shop/urls.py
+++ Furniture_shop/Furniture_shop/urls.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.http import HttpResponse
+
 
 urlpatterns = [
+    path('test/', lambda request: HttpResponse('It works!')),
     path('admin/', admin.site.urls),
     path('', include('core.urls')),
     path('order/', include('order.urls')),
